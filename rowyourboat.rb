1abb3f699a16b7025776b5c7503f2f3f04b608bc
mikesmall <michael.l.a.small@gmail.com>
2017-06-29 09:47:16 -0400

    Initial commit with instructions

---
+++ rowyourboat.rb
@@ -0,0 +1,9 @@
+#  Problems for coffee & code Session 12
+
+#  Write a method that converts this array
+
+#             song = [["row", 3], ["your", 1], ["boat", 1]]
+
+#  to this one:
+
+#             ["row", "row", "row", "your", "boat"]
