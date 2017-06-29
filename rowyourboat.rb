aa478a8c3f5559e51d0ed52a64df9f4cd7ab5a81
mikesmall <michael.l.a.small@gmail.com>
2017-06-29 10:03:50 -0400

    Arrays added for finding solution

--- rowyourboat.rb
+++ rowyourboat.rb
@@ -8,7 +8,7 @@
 
 #             ["row", "row", "row", "your", "boat"]
 
-# In-class suggestion 1:
+# Student suggestion 1:
 # 1. Make a new array
 # 2. Iterate over the provided array
 # 3. Grab the first element of the sub-array
@@ -18,11 +18,17 @@
 # 7. Stop when counter is equal to value of second element of sub-array
 # 8. Return the new array
 
-# In-class suggestion 2:
+# Student suggestion 2:
 # 1. Make a new array
 # 2. Iterate over the provided array
 # 3. Multiply the first element of the sub-array by the last element
 # 4. Put the result in a new array
 # 5. Return the new array
 
- 
+song = [["row", 3], ["your", 1], ["boat", 1]]
+sing = []
+
+# Instructor suggests:
+first_subarray = song[0]
+second_subarray = song[1]
+third_subarray = song[2]
