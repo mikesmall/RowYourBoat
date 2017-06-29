9bf01a0943db4a1581f5dfdd8937d763f73ca0c7
mikesmall <michael.l.a.small@gmail.com>
2017-06-29 09:58:28 -0400

    Comments with in-class solution suggestions

--- rowyourboat.rb
+++ rowyourboat.rb
@@ -7,3 +7,22 @@
 #  to this one:
 
 #             ["row", "row", "row", "your", "boat"]
+
+# In-class suggestion 1:
+# 1. Make a new array
+# 2. Iterate over the provided array
+# 3. Grab the first element of the sub-array
+# 4. Initialize a counter to 0
+# 5. Add the first element to the new array
+# 6. Increment the counter by 1
+# 7. Stop when counter is equal to value of second element of sub-array
+# 8. Return the new array
+
+# In-class suggestion 2:
+# 1. Make a new array
+# 2. Iterate over the provided array
+# 3. Multiply the first element of the sub-array by the last element
+# 4. Put the result in a new array
+# 5. Return the new array
+
+ 
